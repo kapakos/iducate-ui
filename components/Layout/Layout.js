c9ff168bb94ba51aaa6ac7660d8441b92215eed0
Petros Kapakos <petros.kapakos1@aexp.com>
2017-03-18 22:51:41 -0400

    renamed Layout file

---
+++ components/Layout/Layout.js
@@ -0,0 +1,23 @@
+/**
+ * @description
+ *
+ * @author pkapako
+ */
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+import { contentWrapper } from '../../styles/contentWrapper';
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header isAuthenticated={this.props.isAuthenticated} />
+        <div {...contentWrapper}>
+          {this.props.children}
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
